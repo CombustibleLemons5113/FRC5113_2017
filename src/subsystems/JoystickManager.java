ef362a122b20039ed9e21376176628fd246949a4
FRC5113 <frc5113@gmail.com>
2017-02-02 19:43:16 -0500

    Shooter, Low shooter, and Servo

--- src/subsystems/JoystickManager.java
+++ src/subsystems/JoystickManager.java
@@ -12,6 +12,8 @@ public class JoystickManager
 	
 	private JoystickButton resetGyro;
 	private JoystickButton servoUp, servoDown;
+	private JoystickButton intakeIn, intakeOut;
+	private JoystickButton lowerOut;
 	
 	public void init()
 	{
@@ -22,6 +24,9 @@ public class JoystickManager
 		
 		servoUp = new JoystickButton(xboxController, 3);
 		servoDown = new JoystickButton(xboxController, 0);
+		intakeIn = new JoystickButton(xboxController, 6);
+		intakeOut = new JoystickButton(xboxController, 5);
+		lowerOut = new JoystickButton(xboxController, 2);
 	}
 	
 	public void update(DriveTrain driveTrain, Shooter shooter) {
@@ -82,6 +87,18 @@ public class JoystickManager
 			shooter.servo.setAngle(180);
 		else if(servoDown.get())
 			shooter.servo.setAngle(0);
+		
+		if(intakeIn.get())
+			shooter.shooterWheel.set(0.5);
+		else if(intakeIn.get())
+			shooter.shooterWheel.set(-0.5);
+		else
+			shooter.shooterWheel.set(0);
+		
+		if(lowerOut.get())
+			shooter.lowerOutput.set(-1);
+		else
+			shooter.lowerOutput.set(0);
 	}
 	
 	public double getXAxis() {
--- src/subsystems/Shooter.java
+++ src/subsystems/Shooter.java
@@ -1,14 +1,19 @@
 package subsystems;
 
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Servo;
 
 public class Shooter
 {
 	public Servo servo;
+	public CANTalon shooterWheel;
+	public CANTalon lowerOutput;
 	
 	public void init()
 	{
-		servo = new Servo(0);
+		servo = new Servo(9);
+		shooterWheel = new CANTalon(392142);
+		lowerOutput = new CANTalon(324234);
 	}
 	
 	public void update()
